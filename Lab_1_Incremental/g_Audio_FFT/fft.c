6623dc99d7f84a68da906d916a291cb25ab8aee9
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-09-15 13:41:54 -0400

    week 3 stuff

--- Lab_1_Incremental/g_Audio_FFT/fft.c
+++ Lab_1_Incremental/g_Audio_FFT/fft.c
@@ -41,6 +41,10 @@
 // Include protothreads
 #include "pt_cornell_rp2040_v1.h"
 
+#include "pico/multicore.h"
+#include "hardware/sync.h"
+#include "hardware/spi.h"
+
 // Define the LED pin
 #define LED 25
 
@@ -53,6 +57,383 @@ typedef signed int fix15;
 #define int2fix15(a) ((fix15)(a << 15))
 #define fix2int15(a) ((int)(a >> 15))
 #define char2fix15(a) (fix15)(((fix15)(a)) << 15)
+
+// Macros for fixed-point arithmetic (faster than floating point)
+typedef signed int fix15;
+#define multfix15(a, b) ((fix15)((((signed long long)(a)) * ((signed long long)(b))) >> 15))
+#define float2fix15(a) ((fix15)((a)*32768.0))
+#define fix2float15(a) ((float)(a) / 32768.0)
+#define absfix15(a) abs(a)
+#define int2fix15(a) ((fix15)(a << 15))
+#define fix2int15(a) ((int)(a >> 15))
+#define char2fix15(a) (fix15)(((fix15)(a)) << 15)
+#define divfix(a, b) (fix15)((((signed long long)(a)) << 15) / (b))
+
+// Direct Digital Synthesis (DDS) parameters
+#define two32 4294967296.0 // 2^32 (a constant)
+#define DAC_Fs 40000       // sample rate
+
+// Frequencies
+#define CHIRP_FREQ 2300.0
+// #define frequency_1 2500.0
+
+// the DDS units - core 1
+// Phase accumulator and phase increment. Increment sets output frequency.
+volatile unsigned int phase_accum_main_1;
+volatile unsigned int phase_incr_main_1 = (unsigned int)((CHIRP_FREQ * two32) / DAC_Fs);
+// the DDS units - core 2
+// Phase accumulator and phase increment. Increment sets output frequency.
+volatile unsigned int phase_accum_main_0;
+volatile unsigned int phase_incr_main_0 = (unsigned int)((CHIRP_FREQ * two32) / DAC_Fs);
+
+// DDS sine table (populated in main())
+#define sine_table_size 256
+fix15 sin_table[sine_table_size];
+
+// Values output to DAC
+int DAC_output_0;
+int DAC_output_1;
+
+// Amplitude modulation parameters and variables
+fix15 max_amplitude = int2fix15(1); // maximum amplitude
+fix15 attack_inc;                   // rate at which sound ramps up
+fix15 decay_inc;                    // rate at which sound ramps down
+fix15 current_amplitude_0 = 0;      // current amplitude (modified in ISR)
+fix15 current_amplitude_1 = 0;      // current amplitude (modified in ISR)
+
+// Timing parameters for beeps (units of interrupts)
+#define CHIRP_ATTACK_TIME 200
+#define CHIRP_DECAY_TIME 200
+#define CHIRP_SUSTAIN_TIME 10000
+// #define BEEP_DURATION 10400
+// #define BEEP_REPEAT_INTERVAL 40000
+
+// space in between chirps and syllables in interrupts
+#define SYLLABLE_SPACE 80
+#define CHIRP_SPACE 10400
+#define SYLLABLE_LENGTH 680
+typedef enum emit_state
+{
+    es_wait_for_syllable,
+    es_wait_for_chirp,
+    es_active,
+    es_paused,
+} emit_state_t;
+
+// State machine variables
+volatile emit_state_t es_core_0 = es_wait_for_syllable;
+volatile unsigned int es_core_0_irq_count = 0;
+volatile unsigned int es_core_0_syllable_count = 0;
+volatile unsigned int es_core_0_pause_count = 0;
+
+volatile emit_state_t es_core_1 = es_wait_for_syllable;
+volatile unsigned int es_core_1_irq_count = 0;
+volatile unsigned int es_core_1_syllable_count = 0;
+volatile unsigned int es_core_1_pause_count = 0;
+
+// SPI data
+uint16_t DAC_data_1; // output value
+uint16_t DAC_data_0; // output value
+
+// DAC parameters (see the DAC datasheet)
+// A-channel, 1x, active
+#define DAC_config_chan_A 0b0011000000000000
+// B-channel, 1x, active
+#define DAC_config_chan_B 0b1011000000000000
+
+// SPI configurations (note these represent GPIO number, NOT pin number)
+#define PIN_MISO 4
+#define PIN_CS 5
+#define PIN_SCK 6
+#define PIN_MOSI 7
+#define LDAC 8
+#define LED 25
+#define SPI_PORT spi0
+
+#define GPIO_PAUSE_CORE_1 14
+#define GPIO_PAUSE_CORE_0 15
+
+// Two variables to store core number
+volatile int corenum_0;
+volatile int corenum_1;
+
+// Global counter for spinlock experimenting
+volatile int global_counter = 0;
+
+// Semaphore
+struct pt_sem core_1_go, core_0_go;
+
+// This timer ISR is called on core 1
+bool repeating_timer_callback_core_1(struct repeating_timer *t)
+{
+    es_core_1_irq_count++;
+
+    if (es_core_1 != es_paused)
+    {
+        if (!gpio_get(GPIO_PAUSE_CORE_1))
+        {
+            es_core_1_pause_count++;
+
+            if (es_core_1_pause_count >= 20000)
+            {
+                es_core_1 = es_paused;
+                es_core_1_irq_count = 0;
+                es_core_1_pause_count = 0;
+            }
+        }
+        else
+        {
+            es_core_1_pause_count = 0;
+        }
+    }
+    else
+    {
+        if (!gpio_get(GPIO_PAUSE_CORE_1))
+        {
+            es_core_1_pause_count++;
+
+            if (es_core_1_pause_count >= 20000)
+            {
+                es_core_1 = es_wait_for_chirp;
+                es_core_1_irq_count = 0;
+                es_core_1_pause_count = 0;
+            }
+        }
+        else
+        {
+            es_core_1_pause_count = 0;
+        }
+    }
+
+    switch (es_core_1)
+    {
+    case es_active:
+        // DDS phase and sine table lookup
+        phase_accum_main_1 += phase_incr_main_1;
+        DAC_output_1 = fix2int15(multfix15(current_amplitude_1,
+                                           sin_table[phase_accum_main_1 >> 24])) +
+                       2048;
+
+        // Ramp up amplitude
+        if (es_core_1_irq_count < CHIRP_ATTACK_TIME)
+        {
+            current_amplitude_1 = (current_amplitude_1 + attack_inc);
+        }
+
+        // Ramp down amplitude
+        else if (es_core_1_irq_count > SYLLABLE_LENGTH - CHIRP_DECAY_TIME)
+        {
+            current_amplitude_1 = (current_amplitude_1 - decay_inc);
+        }
+
+        // Mask with DAC control bits
+        DAC_data_1 = (DAC_config_chan_A | (DAC_output_1 & 0xffff));
+
+        // SPI write (no spinlock b/c of SPI buffer)
+        spi_write16_blocking(SPI_PORT, &DAC_data_1, 1);
+
+        // State transition
+        if (es_core_1_irq_count == SYLLABLE_LENGTH)
+        {
+            if (es_core_1_syllable_count >= 8)
+            {
+                es_core_1 = es_wait_for_chirp;
+                es_core_1_syllable_count = 0;
+            }
+            else
+            {
+                es_core_1 = es_wait_for_syllable;
+                es_core_1_syllable_count++;
+            }
+
+            es_core_1_irq_count = 0;
+        }
+        break;
+    case es_wait_for_chirp:
+        if (es_core_1_irq_count >= CHIRP_SPACE)
+        {
+            current_amplitude_1 = 0;
+            es_core_1 = es_active;
+            es_core_1_irq_count = 0;
+        }
+        break;
+    case es_wait_for_syllable:
+        if (es_core_1_irq_count >= SYLLABLE_SPACE)
+        {
+            current_amplitude_1 = 0;
+            es_core_1 = es_active;
+            es_core_1_irq_count = 0;
+        }
+        break;
+    }
+
+    // retrieve core number of execution
+    corenum_1 = get_core_num();
+
+    return true;
+}
+
+// This timer ISR is called on core 0
+bool repeating_timer_callback_core_0(struct repeating_timer *t)
+{
+    es_core_0_irq_count++;
+
+    if (es_core_0 != es_paused)
+    {
+        if (!gpio_get(GPIO_PAUSE_CORE_0))
+        {
+            es_core_0_pause_count++;
+
+            if (es_core_0_pause_count >= 20000)
+            {
+                es_core_0 = es_paused;
+                es_core_0_irq_count = 0;
+                es_core_0_pause_count = 0;
+            }
+        }
+        else
+        {
+            es_core_0_pause_count = 0;
+        }
+    }
+    else
+    {
+        if (!gpio_get(GPIO_PAUSE_CORE_0))
+        {
+            es_core_0_pause_count++;
+
+            if (es_core_0_pause_count >= 20000)
+            {
+                es_core_0 = es_wait_for_chirp;
+                es_core_0_irq_count = 0;
+                es_core_0_pause_count = 0;
+            }
+        }
+        else
+        {
+            es_core_0_pause_count = 0;
+        }
+    }
+
+    switch (es_core_0)
+    {
+    case es_active:
+        // DDS phase and sine table lookup
+        phase_accum_main_0 += phase_incr_main_0;
+        DAC_output_0 = fix2int15(multfix15(current_amplitude_0,
+                                           sin_table[phase_accum_main_0 >> 24])) +
+                       2048;
+
+        // Ramp up amplitude
+        if (es_core_0_irq_count < CHIRP_ATTACK_TIME)
+        {
+            current_amplitude_0 = (current_amplitude_0 + attack_inc);
+        }
+
+        // Ramp down amplitude
+        else if (es_core_0_irq_count > SYLLABLE_LENGTH - CHIRP_DECAY_TIME)
+        {
+            current_amplitude_0 = (current_amplitude_0 - decay_inc);
+        }
+
+        // Mask with DAC control bits
+        DAC_data_0 = (DAC_config_chan_B | (DAC_output_0 & 0xffff));
+
+        // SPI write (no spinlock b/c of SPI buffer)
+        spi_write16_blocking(SPI_PORT, &DAC_data_0, 1);
+
+        // State transition
+        if (es_core_0_irq_count >= SYLLABLE_LENGTH)
+        {
+            if (es_core_0_syllable_count >= 8)
+            {
+                es_core_0 = es_wait_for_chirp;
+                es_core_0_syllable_count = 0;
+            }
+            else
+            {
+                es_core_0 = es_wait_for_syllable;
+                es_core_0_syllable_count++;
+            }
+
+            es_core_0_irq_count = 0;
+        }
+        break;
+    case es_wait_for_chirp:
+        if (es_core_0_irq_count >= CHIRP_SPACE)
+        {
+            current_amplitude_0 = 0;
+            es_core_0 = es_active;
+            es_core_0_irq_count = 0;
+        }
+        break;
+    case es_wait_for_syllable:
+        if (es_core_0_irq_count >= SYLLABLE_SPACE)
+        {
+            current_amplitude_0 = 0;
+            es_core_0 = es_active;
+            es_core_0_irq_count = 0;
+        }
+        break;
+    }
+
+    // retrieve core number of execution
+    corenum_0 = get_core_num();
+
+    return true;
+}
+
+// This thread runs on core 1
+static PT_THREAD(protothread_core_1(struct pt *pt))
+{
+    // Indicate thread beginning
+    PT_BEGIN(pt);
+    while (1)
+    {
+        // Wait for signal
+        PT_SEM_SAFE_WAIT(pt, &core_1_go);
+        // Turn off LED
+        gpio_put(LED, 0);
+        // Increment global counter variable
+        for (int i = 0; i < 10; i++)
+        {
+            global_counter += 1;
+            sleep_ms(250);
+            printf("Core 1: %d, ISR core: %d\n", global_counter, corenum_1);
+        }
+        printf("\n\n");
+        // signal other core
+        PT_SEM_SAFE_SIGNAL(pt, &core_0_go);
+    }
+    // Indicate thread end
+    PT_END(pt);
+}
+
+// This thread runs on core 0
+static PT_THREAD(protothread_core_0(struct pt *pt))
+{
+    // Indicate thread beginning
+    PT_BEGIN(pt);
+    while (1)
+    {
+        // Wait for signal
+        PT_SEM_SAFE_WAIT(pt, &core_0_go);
+        // Turn on LED
+        gpio_put(LED, 1);
+        // Increment global counter variable
+        for (int i = 0; i < 10; i++)
+        {
+            global_counter += 1;
+            sleep_ms(250);
+            printf("Core 0: %d, ISR core: %d\n", global_counter, corenum_0);
+        }
+        printf("\n\n");
+        // signal other core
+        PT_SEM_SAFE_SIGNAL(pt, &core_1_go);
+    }
+    // Indicate thread end
+    PT_END(pt);
+}
 
 /////////////////////////// ADC configuration ////////////////////////////////
 // ADC Channel and pin
@@ -67,7 +448,7 @@ typedef signed int fix15;
 // Log2 number of samples
 #define LOG2_NUM_SAMPLES 10
 // Sample rate (Hz)
-#define Fs 10000.0
+#define ADC_Fs 10000.0
 // ADC clock rate (unmutable!)
 #define ADCCLK 48000000.0
 
@@ -200,9 +581,11 @@ typedef enum listen_state
     ls_wait,
 } listen_state_t;
 
-listen_state_t listen_state = ls_active;
-unsigned int listen_state_last_time = 0;
-unsigned int chirp_counter = 0;
+volatile listen_state_t listen_state = ls_active;
+volatile unsigned int ls_last_time = 0;
+volatile unsigned int ls_chirp_count_external = 0;
+volatile unsigned int ls_chirp_count_core_0 = 0;
+volatile unsigned int ls_chirp_count_core_1 = 0;
 
 // Runs on core 0
 static PT_THREAD(protothread_fft(struct pt *pt))
@@ -285,8 +668,9 @@ static PT_THREAD(protothread_fft(struct pt *pt))
                 max_fr_dex = i;
             }
         }
+
         // Compute max frequency in Hz
-        max_freqency = max_fr_dex * (Fs / NUM_SAMPLES);
+        max_freqency = max_fr_dex * (ADC_Fs / NUM_SAMPLES);
 
         // Display on VGA
         fillRect(250, 20, 176, 20, BLACK); // red box
@@ -303,30 +687,51 @@ static PT_THREAD(protothread_fft(struct pt *pt))
             drawVLine(59 + i, 479 - height, height, WHITE);
         }
 
-        fillRect(250, 60, 176, 40, RED);
+        fillRect(250, 60, 350, 40, RED);
 
-        unsigned int listen_state_duration = listen_state_last_time - now;
+        unsigned int listen_state_duration = ls_last_time - now;
         switch (listen_state)
         {
         case ls_wait:
-            if (abs(max_freqency - 2300) < 20 && listen_state_duration > 100)
+            if (abs(max_freqency - 2300) < 50 && listen_state_duration > 25)
             {
                 listen_state = ls_active;
-                listen_state_last_time = now;
+                ls_last_time = now;
+
+                bool core_0_chirping = es_core_0 == es_active || es_core_0 == es_wait_for_syllable;
+                bool core_1_chirping = es_core_1 == es_active || es_core_1 == es_wait_for_syllable;
+
+                if (core_0_chirping)
+                {
+                    ls_chirp_count_core_0++;
+                }
+
+                if (core_1_chirping)
+                {
+                    ls_chirp_count_core_1++;
+                }
+
+                if (!(core_0_chirping || core_1_chirping))
+                {
+                    ls_chirp_count_external++;
+                }
             }
             break;
         case ls_active:
             setCursor(250, 80);
-            writeString("chirp detected");
-            if (abs(max_freqency - 2300) > 20 && listen_state_duration > 100)
+
+            sprintf(chirptext, "chirp detected");
+            writeString(chirptext);
+
+            if (abs(max_freqency - 2300) > 50 && listen_state_duration > 100)
             {
                 listen_state = ls_wait;
-                listen_state_last_time = now;
-                chirp_counter++;
+                ls_last_time = now;
+                puts("chirp!");
             }
             break;
         }
-        sprintf(chirptext, "%d %d %d %d", chirp_counter, listen_state, listen_state_last_time, now);
+        sprintf(chirptext, "ext %d c0 %d c1 %d ls %d now %d", ls_chirp_count_external, ls_chirp_count_core_0, ls_chirp_count_core_1, listen_state, now);
         setCursor(250, 60);
         writeString(chirptext);
     }
@@ -349,8 +754,24 @@ static PT_THREAD(protothread_blink(struct pt *pt))
 // Core 1 entry point (main() for core 1)
 void core1_entry()
 {
+
+    // create an alarm pool on core 1
+    alarm_pool_t *core1pool;
+    core1pool = alarm_pool_create(2, 16);
+
+    // Create a repeating timer that calls repeating_timer_callback.
+    struct repeating_timer timer_core_1;
+
+    // Negative delay so means we will call repeating_timer_callback, and call it
+    // again 25us (40kHz) later regardless of how long the callback took to execute
+    alarm_pool_add_repeating_timer_us(core1pool, -25,
+                                      repeating_timer_callback_core_1, NULL, &timer_core_1);
+
+    // Add thread to core 1
+    pt_add_thread(protothread_core_1);
+
     // Add and schedule threads
-    pt_add_thread(protothread_blink);
+    // pt_add_thread(protothread_blink);
     pt_schedule_start;
 }
 
@@ -366,8 +787,6 @@ int main()
     // Initialize stdio
     stdio_init_all();
 
-    printf("good morning");
-
     // Initialize the VGA screen
     initVGA();
 
@@ -380,9 +799,7 @@ int main()
     gpio_set_dir(LED, GPIO_OUT);
     gpio_put(LED, 0);
 
-    ///////////////////////////////////////////////////////////////////////////////
-    // ============================== ADC CONFIGURATION ==========================
-    //////////////////////////////////////////////////////////////////////////////
+#pragma region ADC config
     // Init GPIO for analogue use: hi-Z, no pulls, disable digital input buffer.
     adc_gpio_init(ADC_PIN);
 
@@ -409,20 +826,18 @@ int main()
     // continuously) or > 95 (take samples less frequently than 96 cycle
     // intervals). This is all timed by the 48 MHz ADC clock. This is setup
     // to grab a sample at 10kHz (48Mhz/10kHz - 1)
-    adc_set_clkdiv(ADCCLK / Fs);
+    adc_set_clkdiv(ADCCLK / ADC_Fs);
 
     // Populate the sine table and Hann window table
-    int ii;
-    for (ii = 0; ii < NUM_SAMPLES; ii++)
+    for (int ii = 0; ii < NUM_SAMPLES; ii++)
     {
         Sinewave[ii] = float2fix15(sin(6.283 * ((float)ii) / (float)NUM_SAMPLES));
         window[ii] = float2fix15(0.5 * (1.0 - cos(6.283 * ((float)ii) / ((float)NUM_SAMPLES))));
     }
 
-    /////////////////////////////////////////////////////////////////////////////////
-    // ============================== ADC DMA CONFIGURATION =========================
-    /////////////////////////////////////////////////////////////////////////////////
+#pragma endregion
 
+#pragma region ADC DMA config
     // Channel configurations
     dma_channel_config c2 = dma_channel_get_default_config(sample_chan);
     dma_channel_config c3 = dma_channel_get_default_config(control_chan);
@@ -458,10 +873,73 @@ int main()
         false                                // Don't start immediately.
     );
 
+#pragma endregion
+
+#pragma region DAC config
+    // Initialize SPI channel (channel, baud rate set to 20MHz)
+    spi_init(SPI_PORT, 20000000);
+    // Format (channel, data bits per transfer, polarity, phase, order)
+    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+
+    // Map SPI signals to GPIO ports
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
+
+    // Map LDAC pin to GPIO port, hold it low (could alternatively tie to GND)
+    gpio_init(LDAC);
+    gpio_set_dir(LDAC, GPIO_OUT);
+    gpio_put(LDAC, 0);
+
+    // set up increments for calculating bow envelope
+    attack_inc = divfix(max_amplitude, int2fix15(CHIRP_ATTACK_TIME));
+    decay_inc = divfix(max_amplitude, int2fix15(CHIRP_DECAY_TIME));
+
+    // Build the sine lookup table
+    // scaled to produce values between 0 and 4096 (for 12-bit DAC)
+    for (int ii = 0; ii < sine_table_size; ii++)
+    {
+        sin_table[ii] = float2fix15(2047 * sin((float)ii * 6.283 / (float)sine_table_size));
+    }
+
+    // Initialize the intercore semaphores
+    PT_SEM_SAFE_INIT(&core_0_go, 1);
+    PT_SEM_SAFE_INIT(&core_1_go, 0);
+#pragma endregion
+
+#pragma region Pause button config
+    gpio_init(GPIO_PAUSE_CORE_0);
+    gpio_set_dir(GPIO_PAUSE_CORE_0, GPIO_IN);
+    gpio_pull_up(GPIO_PAUSE_CORE_0);
+
+    gpio_init(GPIO_PAUSE_CORE_1);
+    gpio_set_dir(GPIO_PAUSE_CORE_1, GPIO_IN);
+    gpio_pull_up(GPIO_PAUSE_CORE_1);
+#pragma endregion
+
+    // Create a repeating timer that calls
+    // repeating_timer_callback (defaults core 0)
+    struct repeating_timer timer_core_0;
+
+    // Negative delay so means we will call repeating_timer_callback, and call it
+    // again 25us (40kHz) later regardless of how long the callback took to execute
+    add_repeating_timer_us(-25,
+                           repeating_timer_callback_core_0, NULL, &timer_core_0);
+
+    // Desynchronize the beeps
+    sleep_ms(500);
+
     // Launch core 1
     multicore_launch_core1(core1_entry);
 
     // Add and schedule core 0 threads
     pt_add_thread(protothread_fft);
+    pt_schedule_start;
+
+    // Add core 0 threads
+    pt_add_thread(protothread_core_0);
+
+    // Start scheduling core 0 threads
     pt_schedule_start;
 }
