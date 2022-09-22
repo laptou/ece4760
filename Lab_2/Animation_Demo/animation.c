e7c737e66b83edbce4a5a23eeab0298cc4cafb04
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-09-22 14:19:43 -0400

    add vector math, rename functions, moduralize animation logic
    Co-authored-by: NRBroussard <NRBroussard@users.noreply.github.com>

--- Lab_2/Animation_Demo/animation.c
+++ Lab_2/Animation_Demo/animation.c
@@ -1,7 +1,7 @@
 
 /**
  * Hunter Adams (vha3@cornell.edu)
- * 
+ *
  * This demonstration animates two balls bouncing about the screen.
  * Through a serial interface, the user can change the ball color.
  *
@@ -39,214 +39,297 @@
 // Include protothreads
 #include "pt_cornell_rp2040_v1.h"
 
-// === the fixed point macros ========================================
-typedef signed int fix15 ;
-#define multfix15(a,b) ((fix15)((((signed long long)(a))*((signed long long)(b)))>>15))
+#pragma region fixed point scalar math
+typedef signed int fix15;
+#define multfix15(a, b) ((fix15)((((signed long long)(a)) * ((signed long long)(b))) >> 15))
 #define float2fix15(a) ((fix15)((a)*32768.0)) // 2^15
-#define fix2float15(a) ((float)(a)/32768.0)
-#define absfix15(a) abs(a) 
+#define fix2float15(a) ((float)(a) / 32768.0)
+#define absfix15(a) abs(a)
 #define int2fix15(a) ((fix15)(a << 15))
 #define fix2int15(a) ((int)(a >> 15))
 #define char2fix15(a) (fix15)(((fix15)(a)) << 15)
-#define divfix(a,b) (fix15)(div_s64s64( (((signed long long)(a)) << 15), ((signed long long)(b))))
+#define divfix(a, b) (fix15)(div_s64s64((((signed long long)(a)) << 15), ((signed long long)(b))))
 
-// Wall detection
-#define hitBottom(b) (b>int2fix15(380))
-#define hitTop(b) (b<int2fix15(100))
-#define hitLeft(a) (a<int2fix15(100))
-#define hitRight(a) (a>int2fix15(540))
+#define min(a, b) (a > b ? b : a)
+#define max(a, b) (a > b ? a : b)
+#define clamp(x, a, b) min(max(x, a), b)
+#pragma endregion
 
+#pragma region fixed point vector math
+typedef struct vec2
+{
+  fix15 x;
+  fix15 y;
+} vec2_t;
+
+inline vec2_t new_vec2(fix15 x, fix15 y)
+{
+  return (vec2_t){.x = x, .y = y};
+}
+
+inline vec2_t mul_vec2_fix15(fix15 x, vec2_t v)
+{
+  return (vec2_t){.x = multfix15(v.x, x), .y = multfix15(v.y, x)};
+}
+
+inline vec2_t mul_vec2_vec2(vec2_t a, vec2_t b)
+{
+  return (vec2_t){.x = multfix15(a.x, b.x), .y = multfix15(a.y, b.x)};
+}
+
+inline vec2_t add_vec2(vec2_t a, vec2_t b)
+{
+  return (vec2_t){.x = a.x + b.x, .y = a.y + b.y};
+}
+
+inline vec2_t sub_vec2(vec2_t a, vec2_t b)
+{
+  return (vec2_t){.x = a.x - b.x, .y = a.y - b.y};
+}
+
+inline fix15 dot_vec2(vec2_t a, vec2_t b)
+{
+  return multfix15(a.x, b.x) + multfix15(a.y, b.y);
+}
+
+#pragma endregion
+
+#pragma region wall detection
+
+const fix15 WALL_BOTTOM = int2fix15(380);
+const fix15 WALL_TOP = int2fix15(100);
+const fix15 WALL_LEFT = int2fix15(100);
+const fix15 WALL_RIGHT = int2fix15(540);
+
+#define hitBottom(b) (b > WALL_BOTTOM)
+#define hitTop(b) (b < WALL_TOP)
+#define hitLeft(a) (a < WALL_LEFT)
+#define hitRight(a) (a > WALL_RIGHT)
+
+#pragma endregion
+
+#pragma region rendering parameters
 // uS per frame
-#define FRAME_RATE 33000
+#define FRAME_PERIOD 33000
 
 // the color of the boid
-char color = WHITE ;
+char color = WHITE;
+#pragma endregion
 
-// Boid on core 0
-fix15 boid0_x ;
-fix15 boid0_y ;
-fix15 boid0_vx ;
-fix15 boid0_vy ;
+#pragma region boid state
+typedef struct boid_state
+{
+  vec2_t position;
+  vec2_t velocity;
+} boid_state_t;
 
-// Boid on core 1
-fix15 boid1_x ;
-fix15 boid1_y ;
-fix15 boid1_vx ;
-fix15 boid1_vy ;
+boid_state_t boids[2];
+#pragma endregion
 
 // Create a boid
-void spawnBoid(fix15* x, fix15* y, fix15* vx, fix15* vy, int direction)
+void spawn_boid(boid_state_t *boid, int direction)
 {
   // Start in center of screen
-  *x = int2fix15(320) ;
-  *y = int2fix15(240) ;
+  boid->position.x = int2fix15(320);
+  boid->position.y = int2fix15(240);
+
   // Choose left or right
-  if (direction) *vx = int2fix15(3) ;
-  else *vx = int2fix15(-3) ;
+  if (direction)
+    boid->velocity.x = int2fix15(3);
+  else
+    boid->velocity.x = int2fix15(-3);
+
   // Moving down
-  *vy = int2fix15(1) ;
+  boid->velocity.y = int2fix15(1);
 }
 
 // Draw the boundaries
-void drawArena() {
-  drawVLine(100, 100, 280, WHITE) ;
-  drawVLine(540, 100, 280, WHITE) ;
-  drawHLine(100, 100, 440, WHITE) ;
-  drawHLine(100, 380, 440, WHITE) ;
+void draw_arena()
+{
+  drawVLine(100, 100, 280, WHITE);
+  drawVLine(540, 100, 280, WHITE);
+  drawHLine(100, 100, 440, WHITE);
+  drawHLine(100, 380, 440, WHITE);
 }
 
 // Detect wallstrikes, update velocity and position
-void wallsAndEdges(fix15* x, fix15* y, fix15* vx, fix15* vy)
+// Delta time should be in milliseconds
+void update_boid_motion(boid_state_t *boid, fix15 delta_time)
 {
   // Reverse direction if we've hit a wall
-  if (hitTop(*y)) {
-    *vy = (-*vy) ;
-    *y  = (*y + int2fix15(5)) ;
+  if (hitTop(boid->position.y) || hitBottom(boid->position.y))
+  {
+    boid->velocity.y *= -1;
+    boid->position.y = clamp(boid->velocity.y, WALL_TOP, WALL_BOTTOM);
   }
-  if (hitBottom(*y)) {
-    *vy = (-*vy) ;
-    *y  = (*y - int2fix15(5)) ;
-  } 
-  if (hitRight(*x)) {
-    *vx = (-*vx) ;
-    *x  = (*x - int2fix15(5)) ;
-  }
-  if (hitLeft(*x)) {
-    *vx = (-*vx) ;
-    *x  = (*x + int2fix15(5)) ;
-  } 
 
+  if (hitRight(boid->position.y) || hitLeft(boid->position.y))
+  {
+    boid->velocity.x *= -1;
+    boid->position.x = clamp(boid->velocity.y, WALL_LEFT, WALL_RIGHT);
+  }
+  
   // Update position using velocity
-  *x = *x + *vx ;
-  *y = *y + *vy ;
+  boid->position = add_vec2(boid->position, mul_vec2_fix15(delta_time, boid->velocity));
 }
 
 // ==================================================
 // === users serial input thread
 // ==================================================
-static PT_THREAD (protothread_serial(struct pt *pt))
+static PT_THREAD(protothread_serial(struct pt *pt))
 {
-    PT_BEGIN(pt);
-    // stores user input
-    static int user_input ;
-    // wait for 0.1 sec
-    PT_YIELD_usec(1000000) ;
-    // announce the threader version
-    sprintf(pt_serial_out_buffer, "Protothreads RP2040 v1.0\n\r");
+  PT_BEGIN(pt);
+  // stores user input
+  static int user_input;
+  // wait for 0.1 sec
+  PT_YIELD_usec(1000000);
+  // announce the threader version
+  sprintf(pt_serial_out_buffer, "Protothreads RP2040 v1.0\n\r");
+  // non-blocking write
+  serial_write;
+  while (1)
+  {
+    // print prompt
+    sprintf(pt_serial_out_buffer, "input a number in the range 1-7: ");
     // non-blocking write
-    serial_write ;
-      while(1) {
-        // print prompt
-        sprintf(pt_serial_out_buffer, "input a number in the range 1-7: ");
-        // non-blocking write
-        serial_write ;
-        // spawn a thread to do the non-blocking serial read
-        serial_read ;
-        // convert input string to number
-        sscanf(pt_serial_in_buffer,"%d", &user_input) ;
-        // update boid color
-        if ((user_input > 0) && (user_input < 8)) {
-          color = (char)user_input ;
-        }
-      } // END WHILE(1)
+    serial_write;
+    // spawn a thread to do the non-blocking serial read
+    serial_read;
+    // convert input string to number
+    sscanf(pt_serial_in_buffer, "%d", &user_input);
+    // update boid color
+    if ((user_input > 0) && (user_input < 8))
+    {
+      color = (char)user_input;
+    }
+  } // END WHILE(1)
   PT_END(pt);
 } // timer thread
 
-// Animation on core 0
-static PT_THREAD (protothread_anim(struct pt *pt))
+#pragma region animation
+
+typedef struct animation_thread_state
 {
-    // Mark beginning of thread
-    PT_BEGIN(pt);
+  int core_num;
 
-    // Variables for maintaining frame rate
-    static int begin_time ;
-    static int spare_time ;
+  // for tracking frame rate
+  int last_frame_start;
+  int current_frame_start;
+  int spare_time;
 
-    // Spawn a boid
-    spawnBoid(&boid0_x, &boid0_y, &boid0_vx, &boid0_vy, 0);
+  // for tracking which boids we are animating on this thread
+  // threads should not have overlapping ranges
+  size_t first_boid;
+  size_t last_boid;
+} animation_thread_state_t;
 
-    while(1) {
-      // Measure time at start of thread
-      begin_time = time_us_32() ;      
-      // erase boid
-      drawRect(fix2int15(boid0_x), fix2int15(boid0_y), 2, 2, BLACK);
-      // update boid's position and velocity
-      wallsAndEdges(&boid0_x, &boid0_y, &boid0_vx, &boid0_vy) ;
-      // draw the boid at its new position
-      drawRect(fix2int15(boid0_x), fix2int15(boid0_y), 2, 2, color); 
-      // draw the boundaries
-      drawArena() ;
-      // delay in accordance with frame rate
-      spare_time = FRAME_RATE - (time_us_32() - begin_time) ;
-      // yield for necessary amount of time
-      PT_YIELD_usec(spare_time) ;
-     // NEVER exit while
-    } // END WHILE(1)
+animation_thread_state_t animation_thread_states[2];
+
+void init_animation_thread(animation_thread_state_t *state)
+{
+  for (size_t i = state->first_boid; i < state->last_boid; i++)
+  {
+    spawn_boid(&boids[i], 0);
+  }
+}
+
+void update_animation_thread(animation_thread_state_t *state)
+{
+  // Measure time at start of thread
+  int last_frame_start = state->current_frame_start;
+  int current_frame_start = time_us_32();
+  state->current_frame_start = current_frame_start;
+  int time_delta = current_frame_start - last_frame_start;
+
+  for (size_t i = state->first_boid; i < state->last_boid; i++)
+  {
+    boid_state_t *boid = &boids[i];
+    // erase boid
+    drawRect(fix2int15(boid->position.x), fix2int15(boid->position.y), 2, 2, BLACK);
+    // update boid's position and velocity
+    update_boid_motion(boid, divfix(int2fix15(time_delta), int2fix15(1000)));
+    // draw the boid at its new position
+    drawRect(fix2int15(boid->position.x), fix2int15(boid->position.y), 2, 2, color);
+  }
+
+  // draw the boundaries
+  draw_arena();
+
+  // delay in accordance with frame rate
+  state->spare_time = FRAME_PERIOD - (time_us_32() - current_frame_start);
+}
+
+// Animation on core 0
+static PT_THREAD(protothread_anim0(struct pt *pt))
+{
+  // Mark beginning of thread
+  PT_BEGIN(pt);
+
+  // spawn boids
+  init_animation_thread(&animation_thread_states[0]);
+
+  while (1)
+  {
+    update_animation_thread(&animation_thread_states[0]);
+    PT_YIELD_usec(animation_thread_states[0].spare_time);
+
+    // NEVER exit while
+  } // END WHILE(1)
+
   PT_END(pt);
 } // animation thread
 
-
 // Animation on core 1
-static PT_THREAD (protothread_anim1(struct pt *pt))
+static PT_THREAD(protothread_anim1(struct pt *pt))
 {
-    // Mark beginning of thread
-    PT_BEGIN(pt);
+  // Mark beginning of thread
+  PT_BEGIN(pt);
 
-    // Variables for maintaining frame rate
-    static int begin_time ;
-    static int spare_time ;
+  // spawn boids
+  init_animation_thread(&animation_thread_states[1]);
 
-    // Spawn a boid
-    spawnBoid(&boid1_x, &boid1_y, &boid1_vx, &boid1_vy, 1);
+  while (1)
+  {
+    update_animation_thread(&animation_thread_states[1]);
+    PT_YIELD_usec(animation_thread_states[1].spare_time);
 
-    while(1) {
-      // Measure time at start of thread
-      begin_time = time_us_32() ;      
-      // erase boid
-      drawRect(fix2int15(boid1_x), fix2int15(boid1_y), 2, 2, BLACK);
-      // update boid's position and velocity
-      wallsAndEdges(&boid1_x, &boid1_y, &boid1_vx, &boid1_vy) ;
-      // draw the boid at its new position
-      drawRect(fix2int15(boid1_x), fix2int15(boid1_y), 2, 2, color); 
-      // delay in accordance with frame rate
-      spare_time = FRAME_RATE - (time_us_32() - begin_time) ;
-      // yield for necessary amount of time
-      PT_YIELD_usec(spare_time) ;
-     // NEVER exit while
-    } // END WHILE(1)
+    // NEVER exit while
+  } // END WHILE(1)
+
   PT_END(pt);
 } // animation thread
 
 // ========================================
 // === core 1 main -- started in main below
 // ========================================
-void core1_main(){
+void core1_main()
+{
   // Add animation thread
   pt_add_thread(protothread_anim1);
   // Start the scheduler
-  pt_schedule_start ;
-
+  pt_schedule_start;
 }
 
 // ========================================
 // === main
 // ========================================
 // USE ONLY C-sdk library
-int main(){
+int main()
+{
   // initialize stio
-  stdio_init_all() ;
+  stdio_init_all();
 
   // initialize VGA
-  initVGA() ;
+  initVGA();
 
-  // start core 1 
+  // start core 1
   multicore_reset_core1();
   multicore_launch_core1(&core1_main);
 
   // add threads
   pt_add_thread(protothread_serial);
-  pt_add_thread(protothread_anim);
+  pt_add_thread(protothread_anim0);
 
   // start scheduler
-  pt_schedule_start ;
-} 
+  pt_schedule_start;
+}
